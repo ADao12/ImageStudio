ff90dde5bb3dbf04a5e3684e1f197fcf0557c3b0
qx-win <adao12@qq.com>
2023-07-02 17:59:09 +0800

    支持两个尺寸命名不同文件名

--- ImageMarker/ImageMarker.py
+++ ImageMarker/ImageMarker.py
@@ -1,110 +1,148 @@
-
-import os
-import platform
 import tkinter as tk
-import tkinterdnd2 as tk2
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageTk
+import tkinterdnd2 as tk2
 from tkinterdnd2 import DND_FILES
-print(tk.TkVersion)
+import os
 
 class App(tk2.Tk):
     def __init__(self):
         super().__init__()
-        self.title("图片规格标记器")
-        self.geometry("500x300")
-        
-        # Create widgets
-        self.prefix_label = tk.Label(self, text="Prefix:")
-        self.prefix_label.pack()
-        self.prefix_entry = tk.Entry(self, width=50)
-        self.prefix_entry.insert(0, "款号")
-        self.prefix_entry.pack()
-        
+        self.title("图片尺寸名字修改器")
+        self.geometry("600x400")
+
+        # 创建GUI控件
+        self.size1_label = tk.Label(self, text="尺寸1:")
+        self.size1_entry = tk.Entry(self, width=10)
+        self.filename1_label = tk.Label(self, text="文件名1:")
+        self.filename1_entry = tk.Entry(self, width=20)
+
+        self.size2_label = tk.Label(self, text="尺寸2:")
+        self.size2_entry = tk.Entry(self, width=10)
+        self.filename2_label = tk.Label(self, text="文件名2:")
+        self.filename2_entry = tk.Entry(self, width=20)
+
         self.folder_button = tk.Button(self, text="选择文件夹", command=self.select_folder)
-        self.folder_button.pack()
-        
-        self.start_button = tk.Button(self, text="开始", command=self.filter_files)
-        self.start_button.pack()
-        
-        self.log_text = tk.Text(self)
-        self.log_text.pack(fill=tk.BOTH, expand=True)
-        
-        # Allow drag and drop
-        # self.bind("<<Drop>>", self.drop_event)
-        # self.bind("<<Drag_Enter>>", self.drag_enter_event)
-        # self.bind("<<Drag_Leave>>", self.drag_leave_event)
-        # self.bind("<<Drag_Ongoing>>", self.drag_ongoing_event)
+        self.start_button = tk.Button(self, text="开始", command=self.process_files)
+        self.log_text = tk.Text(self, width=50, height=10)
 
+        # 布局控件
+        self.size1_label.grid(row=0, column=0, padx=5, pady=5)
+        self.size1_entry.grid(row=0, column=1, padx=5, pady=5)
+        self.filename1_label.grid(row=0, column=2, padx=5, pady=5)
+        self.filename1_entry.grid(row=0, column=3, padx=5, pady=5)
+
+        self.size2_label.grid(row=1, column=0, padx=5, pady=5)
+        self.size2_entry.grid(row=1, column=1, padx=5, pady=5)
+        self.filename2_label.grid(row=1, column=2, padx=5, pady=5)
+        self.filename2_entry.grid(row=1, column=3, padx=5, pady=5)
+
+        self.folder_button.grid(row=2, column=0, padx=5, pady=5, sticky="W")
+        self.start_button.grid(row=2, column=3, padx=5, pady=5, sticky="E")
+
+        self.log_text.grid(row=3, column=0, columnspan=4, padx=5, pady=5, sticky="NSEW")
+
+        # 设置默认值
+        self.size1_entry.insert(0, "80x80")
+        self.filename1_entry.insert(0, "款号_色块")
+        self.size2_entry.insert(0, "1200x1200")
+        self.filename2_entry.insert(0, "款号_正")
+
+        # 自适应布局
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+
+        self.relative_path = ""
+
+        # 设置日志文本格式
+        self.folder_path_text_tag = "folder_path_text_tag"
+        self.log_text.tag_config(self.folder_path_text_tag, foreground="blue")
+        self.unprocessed_file_text_tag = "unprocessed_file_text_tag"
+        self.log_text.tag_config(self.unprocessed_file_text_tag, foreground="red")
+
+        # 设置窗口的拖拽事件处理函数
         self.drop_target_register(DND_FILES)
-        self.dnd_bind("<<Drop>>", self.drop_event)
-        
-    def select_folder(self):
-        self.folder_path = filedialog.askdirectory(initialdir=os.getcwd())
-        self.log_text.insert(tk.END, "Selected folder: {}\n".format(self.folder_path))
-        
-    def filter_files(self):
-        prefix = self.prefix_entry.get()
-        if not hasattr(self, "folder_path"):
-            self.log_text.insert(tk.END, "Please select a folder.\n")
-            return
-        
-        # self.log_text.insert(tk.END, "Files in folder:\n")
-        # for filename in os.listfile(self.folder_path):
-            # self.log_text.insert(tk.END, "- {}\n".format(filename))
-        self.process_folder(self.folder_path, prefix)
+        self.dnd_bind("<<Drop>>", self.drag_and_drop_folder)
 
-    def drop_files(self, files):
-        self.folder_path = files[0]
-        self.log_text.insert(tk.END, "Selected folder: {}\n".format(self.folder_path))
-                
-    def drop_event(self, event):
-        self.folder_path = event.data.strip()
-        self.log_text.insert(tk.END, "Selected folder: {}\n".format(self.folder_path))
-        
-    def drag_enter_event(self, event):
-        event.widget.config(bg="gray")
-        
-    def drag_leave_event(self, event):
-        event.widget.config(bg=self.cget("background"))
-        
-    def drag_ongoing_event(self, event):
-        event.widget.config(bg="gray")
-    
-    # 定义检测和重命名文件函数
-    def process_folder(self, folder_path, prefix):
-        for foldername, subfolders, filenames in os.walk(folder_path):
-            for filename in filenames:
-                if filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg') or filename.lower().endswith('.png'):
-                    filepath = os.path.join(foldername, filename)
-                    try:
-                        fp = open(filepath, 'rb')
-                        with Image.open(fp) as im:
-                            width, height = im.size
-                            size = os.path.getsize(filepath)
-                            fp.close()
-                            if (width != 1340 or height != 1785) and (width != 1200 or height != 1200) and (width != 80 or height != 80) and (size < 200000 or size > 2000000):
-                                # new_filename = f"{prefix}_not_match_{filename}"
-                                # os.rename(filepath, os.path.join(foldername, new_filename))
-                                self.log_text.insert(tk.END, f'尺寸不匹配 {filename} 规格： {width} x {height}\n')
-                            elif width == 1200 and height == 1200:
-                                new_filename = f"{prefix}_正.png"
-                                os.rename(filepath, os.path.join(foldername, new_filename))
-                                self.log_text.insert(tk.END, f'Renamed {filename} to {new_filename}\n')
-                            elif width == 80 and height == 80:
-                                new_filename = f"{prefix}_色块.png"
-                                os.rename(filepath, os.path.join(foldername, new_filename))
-                                self.log_text.insert(tk.END, f'Renamed {filename} to {new_filename}\n')
-                            else:
-                                self.log_text.insert(tk.END, f'尺寸不匹配2 {filename} 规格： {width} x {height}\n')
-                    except OSError as e:
-                        fp.close()
-                        self.log_text.insert(tk.END, f'{filename} has a problem: {e}')
-        
+    def drag_and_drop_folder(self, event):
+        if event.widget is self:
+            if event.data:
+                folder_path = event.data.strip()
+                if os.path.isdir(folder_path):
+                    self.log_text.config(state=tk.NORMAL)
+                    self.log_text.insert(tk.END, f"拖拽文件夹: {folder_path}\n", self.folder_path_text_tag)
+                    self.log_text.config(state=tk.DISABLED)
+                    self.folder_path = folder_path
+                    self.relative_path = os.path.relpath(folder_path)
+        else:
+            event.widget.destroy()
+
+
+
+    def select_folder(self):
+        folder_path = filedialog.askdirectory(initialdir=os.getcwd())
+        self.log_text.config(state=tk.NORMAL)
+        self.log_text.insert(tk.END, f"选择文件夹: {folder_path}\n", self.folder_path_text_tag)
+        self.log_text.config(state=tk.DISABLED)
+        self.log_text.config(state=tk.DISABLED)
+        self.folder_path = folder_path
+        self.relative_path = os.path.relpath(folder_path)
+
+    def process_files(self):
+        folder_path = self.folder_path
+        size1 = self.size1_entry.get()
+        filename1 = self.filename1_entry.get()
+        size2 = self.size2_entry.get()
+        filename2 = self.filename2_entry.get()
+
+        # 遍历文件夹
+        for root, dirs, files in os.walk(folder_path):
+            for filename in files:
+                # 检查文件类型是否为图片
+                if not filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    filepath = os.path.join(root, filename)
+                    unprocessed_file_path = os.path.relpath(filepath, self.relative_path)
+                    self.log_text.config(state=tk.NORMAL)
+                    self.log_text.insert(tk.END, f"无法处理的文件: {unprocessed_file_path}\n", self.unprocessed_file_text_tag)
+                    self.log_text.config(state=tk.DISABLED)
+                    continue
+                    
+                # 检查文件大小是否符合要求
+                filepath = os.path.join(root, filename)
+                try:
+                    with Image.open(filepath) as img:
+                        width, height = img.size
+                except:
+                    unprocessed_file_path = os.path.relpath(filepath, self.relative_path)
+                    self.log_text.config(state=tk.NORMAL)
+                    self.log_text.insert(tk.END, f"无法处理的文件: {unprocessed_file_path}\n", self.unprocessed_file_text_tag)
+                    self.log_text.config(state=tk.DISABLED)
+                    continue
+                    
+                if f"{width}x{height}" == size1:
+                    new_filename = filename.replace(os.path.splitext(filename)[0], filename1)
+                    new_filepath = os.path.join(root, new_filename)
+                    os.rename(filepath, new_filepath)
+                    old_path = os.path.relpath(filepath, self.relative_path)
+                    new_path = os.path.relpath(new_filepath, self.relative_path)
+                    self.log_text.config(state=tk.NORMAL)
+                    self.log_text.insert(tk.END, f"{old_path} --> {new_path}\n")
+                    self.log_text.config(state=tk.DISABLED)
+                elif f"{width}x{height}" == size2:
+                    new_filename = filename.replace(os.path.splitext(filename)[0], filename2)
+                    new_filepath = os.path.join(root, new_filename)
+                    os.rename(filepath, new_filepath)
+                    old_path = os.path.relpath(filepath, self.relative_path)
+                    new_path = os.path.relpath(new_filepath, self.relative_path)
+                    self.log_text.config(state=tk.NORMAL)
+                    self.log_text.insert(tk.END, f"{old_path} --> {new_path}\n")
+                    self.log_text.config(state=tk.DISABLED)
+
+
+
 if __name__ == "__main__":
     app = App()
-    # if platform.system() == "Windows":
-    #     from distutils.core import setup
-    #     import py2exe
-    #     setup(console=["app.py"])
     app.mainloop()
